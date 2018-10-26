546e9353d03d65cdfe421355a23fa87717be35d3
Egor Kuzmichev <egor@kuzmichev.design>
2018-10-26 13:31:45 +0300

    fixes

--- base/views.py
+++ base/views.py
@@ -3,6 +3,6 @@ from .models import Url
 
 # Create your views here.
 def index(request):
-    urls=Url.objects.all()
+    urls=Url.objects.order_by('tag')
     context = {'urls': urls}
     return render (request, 'base/index.html',context)
