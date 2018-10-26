1631bbec9ad6527017f677c95d52e6f0c84a70e9
Egor Kuzmichev <egor@kuzmichev.design>
2018-10-26 23:19:16 +0300

    done

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True)
     title = models.CharField(max_length=100, null=False, default='')
@@ -19,10 +20,8 @@ class Url(models.Model):
     url = models.CharField(max_length=500)
     title = models.CharField(max_length=500, null=True)
     tag = models.CharField(max_length=100, null=True)
-    many_tag = models.ManyToManyField(Tag,blank=True)
-    areas = models.ManyToManyField(Area,blank=True)
+    many_tag = models.ManyToManyField(Tag,blank=True, verbose_name='Тип')
+    areas = models.ManyToManyField(Area,blank=True, verbose_name='Раздел')
 
     def __str__(self):
         return self.title
-
-# Create your models here.
--- base/views.py
+++ base/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import *
 
+from django_filters import FilterSet
+
+
+class UrlFilter(FilterSet):
+    class Meta:
+        model = Url
+        fields = {
+            'many_tag': ['exact'],
+            'areas': ['exact'],
+            'tag': ['exact'],
+            'title': ['contains']
+        }
+
+
 # Create your views here.
 def index(request):
-    tags=Tag.objects.all()
-    urls=Url.objects.filter(many_tag=tags.first()).order_by('tag')
-    context = {'tags': tags, 'urls': urls}
+    f = UrlFilter(request.GET, queryset=Url.objects.all())
+    context = {'filter': f}
     return render (request, 'base/index.html',context)
 
 def tag(request,tag):
