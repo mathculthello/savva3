96c95e5cfa5a8163bd8f1240db211010a4af683c
Egor Kuzmichev <egor@kuzmichev.design>
2018-10-26 12:57:07 +0300

    pages

--- savva3/urls.py
+++ savva3/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('base/', include('base.urls')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
