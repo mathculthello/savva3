81589421422e8a0125972a5b73ad9fa66bc854e0
Egor Kuzmichev <egor@kuzmichev.design>
2018-10-26 14:13:34 +0300

    admin

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Url
 # Register your models here.
+from django.utils.html import format_html
 
-admin.site.register(Url)
+
+
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ('title', 'link', 'tag')
+    def link(self,obj):
+        return format_html("<a target='_blank' href='{url}'>{url}</a>", url=obj.url)
+
+
+
+
+admin.site.register(Url, UrlAdmin)
