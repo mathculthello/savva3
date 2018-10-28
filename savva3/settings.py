51c6d9e5e6c9224d4eedf35a0989940764f59478
Egor Kuzmichev <egor@kuzmichev.design>
2018-10-28 18:32:28 +0300

    adultjokes

--- savva3/settings.py
+++ savva3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 
-MARKDOWN_FILTER_WHITELIST_TAGS = ['p']
+MARKDOWN_FILTER_WHITELIST_TAGS = ['p','ul','li']
 
 # Application definition
 
