a189e6f2a612b89616badc2bb1fbb1f2a739b1ee
Egor Kuzmichev <egor@kuzmichev.design>
2018-10-29 05:55:08 +0300

    filter

--- base/filters.py
+++ base/filters.py
@@ -1,11 +1,18 @@
-from django_filters import FilterSet
-from .models import Url
+import django_filters
+from django_filters import widgets
+from .models import Url, Area, Tag
+from django.db import models
 
-class UrlFilter(FilterSet):
+class UrlFilter(django_filters.FilterSet):
+    areas = django_filters.ModelChoiceFilter(
+        queryset=Area.objects.all(),
+        label="Разделы",
+        widget=widgets.LinkWidget())
+    many_tag = django_filters.ModelChoiceFilter(
+        queryset=Tag.objects.all(),
+        label="Тип",
+        widget=widgets.LinkWidget())
+
     class Meta:
         model = Url
-        fields = {
-            'many_tag': ['exact'],
-            'areas': ['exact'],
-            'title': ['icontains']
-        }
+        fields = ['areas','many_tag',]
