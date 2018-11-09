4b00f1f520820144abf5f1fca0d53f51d494cab6
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-10 02:13:33 +0300

    qs library

--- webpack-src/Base/App.js
+++ webpack-src/Base/App.js
@@ -4,21 +4,21 @@ import Areas from './components/Areas';
 import Set from './components/Set';
 import { Container, Row, Col } from 'reactstrap';
 
-
+import qs from 'qs';
 
 class App extends React.Component {
 
 render() {
-  let params = new URLSearchParams(this.props.location.search);
+  let params=qs.parse(this.props.location.search,{ ignoreQueryPrefix: true });
+//  let params = new URLSearchParams(this.props.location.search);
 
-  console.log(params.get('area'))
   return(
       <Row>
         <Col md="3">
-          <Areas area={params.get('area')}/>
+          <Areas area={params.area}/>
         </Col>
         <Col md="9">
-          <Set area={params.get('area')}/>
+          <Set area={params.area}/>
         </Col>
       </Row>
   )
