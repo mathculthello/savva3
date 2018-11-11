2b132116700c1557cef1ccf09f746d3e7b205009
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-12 02:29:05 +0300

    cleanup

--- base/admin.py
+++ base/admin.py
@@ -4,14 +4,16 @@ from .models import *
 from django.utils.html import format_html
 
 
-
+# View for admin page
 class UrlAdmin(admin.ModelAdmin):
     readonly_fields=('title', 'status_code')
     list_display = ('title', 'link', 'tags','status_code')
 
     list_filter = ('many_tag','areas',)
+
     def link(self,obj):
         return format_html("<a target='_blank' href='{url}'>{url}</a>", url=obj.url)
+
     def tags(self,obj):
         x=''
         for a in obj.many_tag.all():
