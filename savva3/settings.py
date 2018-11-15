9d8517ce5b3ec155edfa4ebfbabc8d87723559d3
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-15 23:19:52 +0300

    База данных перенесена из кодовой базы

--- savva3/settings.py
+++ savva3/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'savva3.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
     }
 }
 
