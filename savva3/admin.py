3dae007b0b05d742c9fc6f4a6ba9d5f7bcd3d5d6
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-17 01:37:54 +0300

    fixes, deleted settings, context_processors

--- savva3/admin.py
+++ savva3/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Setting
-
-admin.site.register(Setting)
--- savva3/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Setting(models.Model):
-    key = models.CharField(max_length=50, unique=True)
-    value = models.TextField()
-
-    def __str__(self):
-        return self.key
