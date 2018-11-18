aae04152ef5d3f6e69e864d048800011d4d33901
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-18 15:07:26 +0300

    redirects

--- savva3/urls.py
+++ savva3/urls.py
@@ -24,22 +24,43 @@ from django.contrib.sitemaps.views import sitemap
 from .sitemaps import sitemaps
 from django.views.generic import TemplateView
 #handler404 = 'savva3.views.handle404'
+from django.urls import reverse_lazy
+
+
+from django.views.generic import RedirectView
+
+
+
 
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
+
+def redirect_permanent(name):
+    return RedirectView.as_view(url=reverse_lazy(name), permanent=True)
+
+
+
 urlpatterns = [
+    # First, redirects
+    path('books/', redirect_permanent('base:books')),
+
+
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', views.index, name='index'),
     path(r'graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('base/', include('base.urls')),
+
     path('jokes/', include('jokes.urls')),
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
     path('features/', include('features.urls')),
     path('404/', views.return404, name='404'),
     path('sitemap/', views.sitemap, name='sitemap'),
+
+
+    # Static pages
 
     path('savvateev/', views.page, {'tpl': 'pages/savvateev.html'}, name='savvateev'),
     path('book/', views.page, {'tpl':'pages/savva_book.html'}, name='savva_book'),
