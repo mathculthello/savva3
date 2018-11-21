de7b1b64db683d7f578f1ec8f6ed738ca48fa646
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-21 10:59:28 +0300

    counter fix

--- features/templatetags/counter.py
+++ features/templatetags/counter.py
@@ -6,7 +6,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def counter(context):
 	url=context.request.path
-	c=Counter()
-	c.url=url
-	c.save()
+	if not context.request.user.is_staff:
+		c=Counter()
+		c.url=url
+		c.save()
 	return Counter.objects.filter(url=url).count()
