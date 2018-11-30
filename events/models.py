873c6ccb74747a814729dbf2e1ffb4f2f7b51faa
Egor Kuzmichev <egor@kuzmichev.design>
2018-11-30 10:01:51 +0300

    events video

--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.utils import timezone
 from meta.models import ModelMeta
 
-from base.models import Resource
+from base.models import Video
 
 from django.conf import settings
 
@@ -28,7 +28,7 @@ class Event(ModelMeta, models.Model):
     city = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=300, blank=True)
 
-    video = models.ManyToManyField(Resource,blank=True)
+    video = models.ManyToManyField(Video,blank=True)
 
     place = models.CharField(max_length=300, blank=True)
 
