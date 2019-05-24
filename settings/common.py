dc0f953355c0abaa7dfd047af09ecacfeb209d14
Egor Kuzmichev <egor@kuzmichev.design>
2019-05-24 19:35:15 +0300

    fixes

--- settings/common.py
+++ settings/common.py
@@ -15,7 +15,7 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 
@@ -139,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru_RU'
+#LANGUAGE_CODE = 'ru_RU'
 
 TIME_ZONE = 'UTC'
 
