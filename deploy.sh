3f4b2418f3cb335e7c801402c52fcaa469ffc1c4
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-11-14 16:08:33 +0000

    set domain to savvateev.xyz

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-export DOMAIN=savva.bhyve.cloud
+export DOMAIN=savvateev.xyz
 export CONFIG=./settings/production.py
 export CONFIG_TAG=$(shasum $CONFIG -a 512 | cut -c1-16)
 docker stack deploy -c stack.yml -c traefik.yml savva3
